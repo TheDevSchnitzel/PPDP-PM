2d41acd87aa7adc2efb46e1c2cabbb0a2fce11f8
Alexander Schnitzler <Alexander.Schnitzler2@rwth-aachen.de>
2020-10-10 17:32:17 +0200

    Nav accordion

--- anonymization/views.py
+++ anonymization/views.py
@@ -11,7 +11,11 @@ from wsgiref.util import FileWrapper
 
 
 def anonymization_main(request):
+    event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
+    temp_path = os.path.join(settings.MEDIA_ROOT, "temp")
+
     if request.method == 'POST':
+        reqValues = extractHttpRequestValues(request)
 
         return render(request, 'anonymization_main.html', {'log_name': settings.EVENT_LOG_NAME, 'values': '', 'outputs': ''})
 
@@ -19,3 +23,11 @@ def anonymization_main(request):
     else:
         return render(request, 'anonymization_main.html', {'log_name': settings.EVENT_LOG_NAME, 'values':'', 'outputs':''})
 
+
+def extractHttpRequestValues(request):
+    values = {}
+    values['OP_Operation'] = request.POST['DDMB_anon_operation']
+    values['OP_Level'] = request.POST['DDMB_anon_applLevel']
+    values['OP_Target'] = request.POST['DDMB_anon_target']
+
+    return values
