2d2949f4563a70a0c8151c90bbbe8b84cd5bdcb6
Alexander 'DevSchnitzel' Schnitzler <Alexander.Schnitzler@outlook.com>
2020-12-21 20:12:51 +0100

    PrivacyAnalysis BugFix SameOrigin

--- privacy_analysis/views.py
+++ privacy_analysis/views.py
@@ -132,7 +132,9 @@ def privacy_analysis_main(request):
                 print(getDataUtilitySettings(reqConfData))
 
                 # Total data utility
+                print("1")
                 utility = getDataUtilityValue(event_log, event_log_backup, getDataUtilitySettings(reqConfData))
+                print("7")
                 return HttpResponse(json.dumps({"Utility": utility}), content_type='application/json')
 
             elif(request.GET['analysis'] == 'disclosureRisk'):
@@ -164,7 +166,7 @@ def getDataUtilityValue(origLogPath, privLogPath, settings):
     time_accuracy = settings['DU_TimeAccuracy'].lower()  # original, seconds, minutes, hours, days
     event_attributes = list(set(settings['DU_EventAttributes']))  # Make list unique
     # these life cycles are applied only when all_lif_cycle = False
-    life_cycle = list(set(settings['DU_LifeCycle']))  # Make list unique
+    life_cycle = ['complete', '', 'COMPLETE']#list(set(settings['DU_LifeCycle']))  # Make list unique
     # when life cycle is in trace attributes then all_life_cycle has to be True
     all_life_cycle = settings['DU_IsAllLifeCycle']
 
@@ -172,20 +174,21 @@ def getDataUtilityValue(origLogPath, privLogPath, settings):
     privacy_log = xes_importer.apply(privLogPath)
     from_same_origin = settings['DU_IsFromSameOrigin']  # when both event logs drived from the same original event logs
 
+ 
     sms = SMS()
-    logsimple, traces, sensitives = sms.create_simple_log_adv(original_log, event_attributes, life_cycle, all_life_cycle, sensitive, time_accuracy)
-    logsimple_2, traces_2, sensitives_2 = sms.create_simple_log_adv(privacy_log, event_attributes, life_cycle, all_life_cycle, sensitive, time_accuracy)
+    logsimple, traces, sensitives = sms.create_simple_log_adv(original_log,event_attributes,life_cycle,all_life_cycle,sensitive,time_accuracy)
+    logsimple_2, traces_2, sensitives_2 = sms.create_simple_log_adv(privacy_log,event_attributes,life_cycle,all_life_cycle,sensitive,time_accuracy)
 
-    # log 1 convert to char
-    map_dict_act_chr, map_dict_chr_act = sms.map_act_char(traces)
-    simple_log_char_1 = sms.convert_simple_log_act_to_char(traces, map_dict_act_chr)
+    #log 1 convert to char
+    map_dict_act_chr,map_dict_chr_act = sms.map_act_char(traces)
+    simple_log_char_1 = sms.convert_simple_log_act_to_char(traces,map_dict_act_chr)
 
-    # log 2 convert to char
-    if from_same_origin:  # use the same mapping
-        simple_log_char_2 = sms.convert_simple_log_act_to_char(traces_2, map_dict_act_chr)
+    #log 2 convert to char
+    if from_same_origin: #use the same mapping
+        simple_log_char_2 = sms.convert_simple_log_act_to_char(traces_2,map_dict_act_chr)
     else:
-        map_dict_act_chr_2, map_dict_chr_act_2 = sms.map_act_char(traces_2)
-        simple_log_char_2 = sms.convert_simple_log_act_to_char(traces_2, map_dict_act_chr_2)
+        map_dict_act_chr_2,map_dict_chr_act_2 = sms.map_act_char(traces_2)
+        simple_log_char_2 = sms.convert_simple_log_act_to_char(traces_2,map_dict_act_chr_2)
 
     start_time = time.time()
 
@@ -194,9 +197,9 @@ def getDataUtilityValue(origLogPath, privLogPath, settings):
     # log_freq_2 , log_only_freq_2 = my_emd.log_freq(traces_2)
 
     log_freq_1, log_only_freq_1 = my_emd.log_freq(simple_log_char_1)
-    log_freq_2, log_only_freq_2 = my_emd.log_freq(simple_log_char_2)
+    log_freq_2 , log_only_freq_2 = my_emd.log_freq(simple_log_char_2)
 
-    cost_lp = my_emd.emd_distance_pyemd(log_only_freq_1, log_only_freq_2, log_freq_1, log_freq_2)
+    cost_lp = my_emd.emd_distance_pyemd(log_only_freq_1,log_only_freq_2,log_freq_1,log_freq_2)
     # cost_lp = my_emd.emd_distance(log_freq_1,log_freq_2)
 
     data_utility = 1 - cost_lp
